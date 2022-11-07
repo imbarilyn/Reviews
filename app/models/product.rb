01784f556f534ea8f5f324ce81a10c9a0dd7f3c5
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-07 21:14:18 +0300

    done with most of the deliverables

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :users,  through: :reviews
     has_many :reviews
-    
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating:star_rating, comment:comment, product_id: self.id, user_id:user.id)
+    end
+
+    def print_all_reviews
+        self.reviews.map  do |review| 
+             puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+    end
+
+        def average_rating
+            self.reviews.average(:star_rating).to_f
+
+        end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
     has_many :products,  through: :reviews
     has_many :reviews 
+
+    def favorite_product
+        self.reviews.maximum(:star_rating)
+
+    end
+
+    def remove_reviews(product)
+        
+
+
+    end
 end
